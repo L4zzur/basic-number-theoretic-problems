a522c0b00cc819708bf2e5c64e944aa5a2ea485c
L4ZZURPC\L4zzur <popovnikita01@gmail.com>
2022-02-02 00:53:09 +0200

    shitcode init

---
+++ quadratic_sieve.py
@@ -0,0 +1,112 @@
+def FactorBaseGenerator(t):
+    import sympy
+    factorBase = [-1]
+    amount = 1
+    num = 2
+    while True:
+        if amount == t:
+            break
+        else:
+            if sympy.isprime(num):
+                factorBase.append(num)
+                amount += 1
+            num += 1
+    return factorBase
+
+def NumberIsSmooth(x, base):
+    if x == 0:
+        return False
+    for p in base:
+        if x < 0:
+            x //= p
+        elif p != -1:
+            while x % p == 0:
+                x //= p
+    if x <= max(base):
+        return True
+    else:
+        return False
+
+def SmoothSplit(x, base):
+    e = []
+    for p in base:
+        k = 0
+        if x < 0:
+            x //= p
+            k += 1
+        elif p != -1:
+            while x % p == 0:
+                x //= p
+                k += 1
+        e.append(k)
+    return e
+
+def main():
+    import math
+    import random
+    import time
+    start = time.time()
+    # n = int(input('Введите число n: '))
+    n = 24961344
+    #t = int(input('Введите количество простых чисел для фактор базы: '))
+    m = int(math.sqrt(n))
+
+    #factorbase = FactorBaseGenerator(t)
+    factorbase = [-1, 2, 3, 5, 7, 11, 13, 17, 19, 23]
+    try_count = 0
+    # lsx = [12, 2522, 5794, -1, 4, 156, 102, -6, 2, 9124, 34]
+    while True:
+        try_count += 1
+        print(f'########### Попытка №{try_count} ###########')
+        e, v, numpairs = [], [], []
+        while len(numpairs) < len(factorbase) + 1:
+            x = random.randrange(n)
+            b = pow(x + m, 2) - n
+            if NumberIsSmooth(b, factorbase):
+                ei = SmoothSplit(b, factorbase)
+                vi = [i % 2 for i in ei]
+                e.append(ei)
+                v.append(vi)
+                numpair = [x + m, b]
+                numpairs.append(numpair)
+                print('x = ', x)
+            if x > 0:
+                x = -x
+            else:
+                x = abs(x) + 1
+        #print(f"Пары: {numpairs}\n", f"Количество пар: {len(numpairs)}\n")
+
+        x = 1
+        for numpair in numpairs:
+            x *= numpair[0] % n
+
+        T = []
+        for i in range(len(factorbase) + 1):
+            #print(v[i], sum(v[i]))
+            if sum(v[i]) % 2 == 0:
+                T.append(i)
+
+        l = []
+        for i in range(len(factorbase)):
+            lj = 0
+            if i in T:
+                for j in range(len(factorbase)):
+                    lj += e[i][j]
+            lj //= 2
+            l.append(lj)
+        print(l)
+
+        y = 1
+        for j in range(len(factorbase)):
+            y *= pow(factorbase[j], l[j], n) % n
+        
+        if ((x % n) == (y % n)) or ((x % n) == (-y % n)):
+            continue
+        elif math.gcd(x - y, n) != 1:
+            print('делитель', math.gcd(x - y, n))
+            end = time.time()
+            print('выполнено за', end - start)
+            return 0
+
+if __name__ ==  "__main__":
+    main()
---
+++ random_squares.py
@@ -0,0 +1,78 @@
+def FactorBaseGenerator(t):
+    import sympy
+    factorBase = []
+    amount = 0
+    num = 2
+    while True:
+        if amount == t:
+            break
+        else:
+            if sympy.isprime(num):
+                factorBase.append(num)
+                amount += 1
+            num += 1
+    return factorBase
+
+def NumberIsSmooth(x, base):
+    if x == 0:
+        return False
+    for p in base:
+        while x % p == 0:
+            x //= p
+    if x <= max(base):
+        return True
+    else:
+        return False
+
+
+
+def main():
+    import sympy
+    import math
+    import random
+
+    n = int(input('Введите число n: '))
+    if sympy.isprime(n):
+        print(f'{n} - просто число')
+        return 0
+    t = int(input('Введите количество простых чисел для фактор базы: '))
+
+    factorbase = FactorBaseGenerator(t)
+
+    a = random.randint(1, n)
+    b = random.randint(1, n)
+
+    try_count = 0
+
+    while True:
+        try_count += 1
+        print(f'########### Попытка №{try_count} ###########')
+
+        numpairs = []
+        while len(numpairs) < len(factorbase) + 1:
+            if pow(a, 2, n) == b and NumberIsSmooth(b, factorbase):
+                a_b = [a, b]
+                numpairs.append(a_b)
+            b += 1
+            if b > n:
+                a = random.randint(1, n)
+                b = random.randint(1, n)
+        print(f"Пары: {numpairs}\n", f"Количество пар: {len(numpairs)}\n")
+        x = 1
+        y = 1
+        for numpair in numpairs:
+            x *= numpair[0]
+            y *= numpair[1]
+        
+        y = int(math.sqrt(y) % n)
+        x %= n
+
+        if pow(x, 2, n) == pow(y, 2, n) and x != y and x != (-y + n):
+            result = math.gcd(abs(x - y), n)
+            if 1 < result < n:
+                print(
+                f'Результат: {result}\nПроверка: {n} / {result} = {n / result}')
+                return 0
+
+if __name__ ==  "__main__":
+    main()
